f039a78724360131efcf4a87305893bcdfae4ff9
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-06-04 03:08:38 +0530

    added output variables that print on the console

--- day5/day4_variable.tf
+++ day5/day4_variable.tf
@@ -22,6 +22,11 @@ variable "enviornment"{
     type = string
     default = "dev"
 }
+variable "region"{
+    description = "AWS region to deploy resources"
+    type = string
+    default = "ap-south-1"
+}
 locals{
     env = var.enviornment 
     bucket_name = "${var.enviornment}-bucket"
@@ -36,6 +41,7 @@ provider "aws" {
 # ** create a s3 bucket
 resource "aws_s3_bucket" "first_bucket" {
     bucket = "my-tf-test-bucket-12345654321"
+    region = var.region
     tags ={
         Name = local.bucket_name
         Environment = var.enviornment
@@ -44,16 +50,31 @@ resource "aws_s3_bucket" "first_bucket" {
 
 resource "aws_vpc" "sample"{
     cidr_block = "10.0.1.0/16"
+    region = var.region
     tags = {
         Name = local.vpc_name
         Environment = var.enviornment
     }
 }
-resource "aws_ec2_instance" "example" {
+resource "aws_instance" "example" {
     ami = "ami-0c02fb55956c7d316"
     instance_type = "t2.micro"
+    region = var.region
     tags = {
         Name = local.ec2_name
         Environment = var.enviornment
     }
+}
+
+output "vpc_id" {
+    value = aws_vpc.sample.id
+
+}
+output "ec2_id" {
+    value = aws_instance.example.id
+
+}
+output "bucket_id" {
+    value = aws_s3_bucket.first_bucket.id
+
 }
