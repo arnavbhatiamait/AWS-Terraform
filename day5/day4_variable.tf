fbdb20816f521d125bad55c0872a3c5f5f0360a4
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-06-04 03:00:55 +0530

    used variable in day 4 file

---
+++ day5/day4_variable.tf
@@ -0,0 +1,53 @@
+# ! configuring s3 for saving state file
+terraform{
+    backend "s3" {
+        bucket = "my-tf-state-bucket-123456543210"
+        key    = "dev/terraform.tfstate"
+        region = "ap-south-1"
+        encrypt = true
+        use_lockfile=true
+    }
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 6.0"
+        }
+    }
+}
+
+
+# ! variable 
+variable "enviornment"{
+    description = "Environment for the resources"
+    type = string
+    default = "dev"
+}
+
+# ! configure provider
+provider "aws" {
+    region = "ap-south-1"
+}
+# ** create a s3 bucket
+resource "aws_s3_bucket" "first_bucket" {
+    bucket = "my-tf-test-bucket-12345654321"
+    tags ={
+        Name = "my bucket"
+        Environment = var.enviornment
+    }
+}
+
+resource "aws_vpc" "sample"{
+    cidr_block = "10.0.1.0/16"
+    tags = {
+        Name = "my_vpc"
+        Environment = var.enviornment
+    }
+}
+resource "aws_ec2_instance" "example" {
+    ami = "ami-0c02fb55956c7d316"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "my_ec2_instance"
+        Environment = var.enviornment
+    }
+}
