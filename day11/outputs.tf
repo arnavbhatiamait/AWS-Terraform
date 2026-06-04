79a469ae5ee297b793f1c92ec1b8df9d09b1e699
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-06-04 20:47:35 +0530

    use lower function in locals

---
+++ day11/outputs.tf
@@ -0,0 +1,4 @@
+output "formatted_project_name" {
+    description = "The formatted project name in lowercase"
+    value       = local.formatted_project_name
+}
---
+++ day11/variables.tf
@@ -0,0 +1,5 @@
+variable "project_name"{
+    description = "The name of the project ALPHA "
+    type        = string
+    default     = "Proj ALPHA"
+}
