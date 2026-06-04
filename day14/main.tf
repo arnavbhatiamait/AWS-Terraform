9cb6474a4591b8141dcb8fef24d5bedd9b794a7b
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-06-05 01:25:47 +0530

    added cloudfront origin acces control on the s3 bucket

--- day14/main.tf
+++ day14/main.tf
@@ -10,4 +10,14 @@ resource "aws_s3_public_access_block" "block"{
     ignore_public_acls = true
     restrict_public_buckets = true
 }
-# ! origin access control
+# ! origin access control -> used to acces the private bucket from cloudfront distribution. It allows cloudfront to access the bucket securely without exposing it to the public. It ensures that only cloudfront can access the bucket, preventing unauthorized access and enhancing security.
+resource "aws_cloudfront_origin_access_control" "oac"{
+    name = "demo-oac"
+    description = "Origin Access Control for S3 bucket"
+    signing_behavior = "always"
+    signing_protocol = "sigv4"
+    origin_access_control_origin_type = "s3"
+}
+
+# ! bucket policy -> used to allow cloudfront distribution to access the private bucket. It defines the permissions for the bucket, allowing cloudfront to read the objects in the bucket while keeping it private from public access.
+
