2285a9a8cfd4160bca014ceddd3a19e071dca9e9
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-06-04 15:14:22 +0530

    used set to add second S3 bucket resource with for_each to support dynamic bucket creation

--- day8/main.tf
+++ day8/main.tf
@@ -4,3 +4,9 @@ resource "aws_s3_bucket" "bucket1"{
     bucket = var.bucket_names[count.index]
     tags=var.tags
 }
+
+resource "aws_s3_bucket" "bucket2"{
+   for_each=var.bucket_names_set 
+    bucket = each.value
+    tags=var.tags
+}
