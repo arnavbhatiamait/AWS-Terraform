bec6f5396100e9f93bb385180de80c3b5af2f231
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-06-06 11:31:10 +0530

    included ecnryption to s3

--- day18/locals.tf
+++ day18/locals.tf
@@ -27,3 +27,25 @@ resource "aws_s3_bucket_versioning" "processed_bucket" {
   }
 }
 
+# ! bucket encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "upload_bucket" {
+  bucket = aws_s3_bucket.upload_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "processed_bucket" {
+  bucket = aws_s3_bucket.processed_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
