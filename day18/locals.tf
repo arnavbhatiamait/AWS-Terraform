d83235f98b134578bb6867f6c5e76738a670fe66
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-06-06 12:59:12 +0530

    s3 public acces block and lambda role and policy

--- day18/locals.tf
+++ day18/locals.tf
@@ -49,3 +49,76 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "processed_bucket"
   }
 }
 
+# ! bucket public access block
+resource "aws_s3_bucket_public_access_block" "upload_bucket" {
+  bucket = aws_s3_bucket.upload_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+resource "aws_s3_bucket_public_access_block" "processed_bucket" {
+  bucket = aws_s3_bucket.processed_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# ! Iam role and policy for lambda to access s3 buckets
+
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.project_name}-${var.environment}-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# ! Lambda policy to allow access to S3 buckets
+resource "aws_iam_role_policy" "lambda_policy" {
+    name = "${var.project_name}-${var.environment}-lambda-policy"
+    role = aws_iam_role.lambda_role.id
+    
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:${var.aws_region}:*:*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion"
+        ]
+        Resource = "${aws_s3_bucket.upload_bucket.arn}/*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:PutObjectAcl"
+        ]
+        Resource = "${aws_s3_bucket.processed_bucket.arn}/*"
+      }
+    ]
+  })
+}
