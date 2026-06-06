3427aff6b94ab8a3d32481c9a97618ef0619e1a2
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-06-06 13:16:17 +0530

    complrted day 18 code

--- day18/main.tf
+++ day18/main.tf
@@ -165,3 +165,26 @@ resource "aws_cloudwatch_log_group" "lambda_log_group" {
     name = "/aws/lambda/${aws_lambda_function.image_processor.function_name}"
     retention_in_days = 7
 }
+
+# ! Lambda permission to allow S3 to invoke it
+resource "aws_lambda_permission" "s3_invoke" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.image_processor.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.upload_bucket.arn
+}
+
+# ! S3 event notification to trigger Lambda on object creation in upload bucket
+resource "aws_s3_bucket_notification" "upload_bucket_notification" {
+    bucket = aws_s3_bucket.upload_bucket.id
+    
+    lambda_function {
+        lambda_function_arn = aws_lambda_function.image_processor.arn
+        events              = ["s3:ObjectCreated:*"]
+        filter_prefix       = "" # Trigger for all objects
+        filter_suffix       = "" # Trigger for all objects
+    }
+    
+    depends_on = [aws_lambda_permission.s3_invoke]
+    }
